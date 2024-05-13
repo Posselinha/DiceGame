9452ef50cfa9ded41153084f82e2d97d9028288f
posselinha <gabrielfilipeposs@hotmail.com>
2024-05-12 23:20:13 -0300

    Adicionar arquivos de projeto.

---
+++ DiceGame.cs
@@ -0,0 +1,36 @@
+﻿static class DiceGame
+{
+    private static int userLifes = 3;
+    private static int randomNumber = NumberGenerator._randomNumberGenerator;
+    private static bool gameState = true;
+
+    public static void StartGame()
+    {
+        do
+        {
+            Console.WriteLine($"Você tem '{userLifes}' vidas restantes!");
+            Console.WriteLine("Digite um número para tentar adivinhar!");
+            string? userGuess = Console.ReadLine();
+            GuessValidator.Validator(userGuess, randomNumber);
+
+        } while (gameState);
+        Console.ReadKey();
+    }
+
+    public static void CorrectChoice()
+    {
+        gameState = false;
+        Console.WriteLine("Você ganhou, parabéns! Aperte qualquer tecla para fechar o programa.");
+    }
+
+    public static void WrongChoice()
+    {
+        Console.WriteLine("Você errou!");
+        userLifes--;
+        if(userLifes == 0)
+        {
+            gameState = false;
+            Console.WriteLine("Você morreu! Aperte qualquer tecla para fechar o programa.");
+        }
+    }
+}
---
+++ GuessValidator.cs
@@ -0,0 +1,19 @@
+﻿static class GuessValidator
+{
+    public static void Validator(string? userGuess, int randomNumber)
+    {
+        bool isValidGuess = int.TryParse(userGuess, out int validGuess);
+
+        if(isValidGuess)
+        {
+            if(validGuess == randomNumber)
+            {
+                DiceGame.CorrectChoice();
+            }
+            else
+            {
+                DiceGame.WrongChoice();
+            }
+        }
+    }
+}
